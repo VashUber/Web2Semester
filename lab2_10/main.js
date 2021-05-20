35696484715d2b3328369cd1b5ab43d3d152cef3
Egor Sizov <mrjarokvk@gmail.com>
2021-05-20 21:01:25 +0300

    8 в процессе

--- lab2_10/main.js
+++ lab2_10/main.js
@@ -1,11 +1,17 @@
 let url = 'https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49'
+let title = document.querySelector('.title')
+    description = document.querySelector('.description')
+    director = document.querySelector('.director')
+    date = document.querySelector('.date')
 fetch(url)
   .finally(alert('запрос выполнен'))
   .then(response => response.json())
-  .then(function(out){
-    document.querySelector('.title').innerText = `Title: ${out.title} (${out.original_title})`
-    document.querySelector('.description').innerText = `Description: ${out.description}`
-    document.querySelector('.director').innerText = `Director: ${out.director}`
-    document.querySelector('.date').innerText = `Release date: ${out.release_date}`
-  })
+  .then(out => output(out))
   .catch(error => console.log(error));
+
+function output(out){
+  title.innerText = `${out.title} (${out.original_title})`
+  description.innerText = `Description: ${out.description}`
+  director.innerText = `Director: ${out.director}`
+  date.innerText = `Release date: ${out.release_date}`
+}
