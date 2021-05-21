eaf1e97b2a1f169f5772951bc607badc24ba71ff
Egor Sizov <mrjarokvk@gmail.com>
2021-05-21 16:46:44 +0300

    8 (fix и часы)

--- lab2_8/main.js
+++ lab2_8/main.js
@@ -1,16 +1,11 @@
-let shine = document.querySelector('.shine')
-    but = document.querySelector('.button')
-    lamp = document.querySelector('.lamp-up')
-    count = 0
+let but = document.querySelector('.button')
+    menu = document.querySelector('.BurgerMenu')
+    lamp = document.querySelector('.Lamp')
 
 but.onclick = () =>{
-    if (count % 2 == 0){
-        shine.style.display = 'unset'
-        lamp.style.fill = '#f5ea56'
-    }
-    else{
-        shine.style.display = 'none'
-        lamp.style.fill = '#000'
-    }
-    count+=1
+    lamp.classList.toggle("lamp-active")
+}
+
+menu.onclick = () =>{
+    menu.classList.toggle("menu-active")
 }
