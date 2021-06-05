d5e6cb66275e14969650614dc742adc8bed0b17e
Egor Sizov <mrjarokvk@gmail.com>
2021-06-06 00:12:21 +0300

    try 11 lab

---
+++ lab2_11/app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from './components/Header/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header 
+  }
+}
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  #app{
+    max-width: 1920px;
+    margin: 0 auto;
+    font-size: 1em;
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
---
+++ lab2_11/app/src/components/Header/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="header">
+        <div class="logo">
+            <img src="../../assets/logo.svg" alt="" class="logo__img">
+        </div>
+        <div class="title">
+            <h1>Kanban</h1>
+        </div>
+        <div class="dark-mode">123</div>
+    </header>
+</template>
+
+<script>
+
+export default({
+    name: 'Header'    
+})
+</script>
+
+
+<style src='./header.css'>
+
+</style>
